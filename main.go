8607ea2304dbc849ba67f6309bafc3c57980e822
EgorSlavenkov <bakketpw@mail.ru>
2025-01-31 19:55:28 +0500

    two new funcs: createUser, deleteAllUsers

--- main.go
+++ main.go
@@ -15,27 +15,34 @@ import (
 type apiConfig struct {
 	fileserverHits atomic.Int32
 	db             *database.Queries
+	platform       string
 }
 
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
+	mux := http.NewServeMux()
+
 	godotenv.Load()
 	dbURL := os.Getenv("DB_URL")
 	if dbURL == "" {
 		log.Fatal("DB_URL must be set")
 	}
+	platform := os.Getenv("PLATFORM")
+	if platform == "" {
+		log.Fatal("PLATFORM must be set")
+	}
+
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
 		log.Fatalf("error connecting to the database: %v", err)
 	}
 	dbQueries := database.New(db)
 
-	const filepathRoot = "."
-	const port = "8080"
-	mux := http.NewServeMux()
-
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
 		db:             dbQueries,
+		platform:       platform,
 	}
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -45,6 +52,7 @@ func main() {
 	mux.Handle("/app/", http.StripPrefix("/app/", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
+	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
